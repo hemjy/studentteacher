0309d7c871e1d96355f23c58f108221c96fd1036
Hashim <hashimabdulmujib@gmail.com>
2023-08-15 08:58:01 +0100

    add database context and config

---
+++ StudentTeacher.Infrastructure/Data/Configurations/StudentConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using StudentTeacher.Core.Entities;
+
+namespace StudentTeacher.Infrastructure.Data.Configurations
+{
+    internal class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(d => d.Id);
+        }
+    }
+}
---
+++ StudentTeacher.Infrastructure/Data/Context/StudentTeacherContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using StudentTeacher.Core.Entities;
+using System.Reflection;
+
+namespace StudentTeacher.Infrastructure.Data.Context
+{
+    public class StudentTeacherContext: DbContext
+    {
+        public DbSet<Student> Students => Set<Student>();
+        public DbSet<Teacher> Teachers => Set<Teacher>();
+        public StudentTeacherContext(DbContextOptions<StudentTeacherContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder
+                .Properties<DateTimeOffset>()
+                .HaveConversion<DateTimeOffsetToStringConverter>(); // SqlLite workaround for DateTimeOffset sorting
+
+            configurationBuilder
+                .Properties<decimal>()
+                .HaveConversion<double>(); // SqlLite workaround for decimal aggregations
+        }
+    }
+}
