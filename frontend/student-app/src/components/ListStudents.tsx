a65ce511f0f752de5fb3db98ec42de3bd32deb3c
Hashim <hashimabdulmujib@gmail.com>
2023-08-16 21:18:31 +0100

    style list student component

--- frontend/student-app/src/components/ListStudents.tsx
+++ frontend/student-app/src/components/ListStudents.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import "./ListStudent.css";
 
 interface Student {
     name: string;
@@ -11,36 +12,31 @@ interface Student {
 const ListStudents: React.FC = () => {
   
     const [students, setStudents] = useState<Student[]>([]);
-    let headers = new Headers();
-
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
-  
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-    headers.append('Access-Control-Allow-Credentials', 'true');
-  
-    headers.append('GET', 'POST');
     useEffect(() => {
       // Fetch data from the API endpoint
-      fetch('http://localhost:5055/api/student/all?pagenumber=1', {
-        method: 'GET',
-        headers,
-    })
+      fetch('http://localhost:5055/api/student/all?pagenumber=1')
         .then((response) => response.json())
-        .then((data) => setStudents(data))
+        .then((data) => setStudents(data.data))
         .catch((error) => console.error('Error fetching data:', error));
     }, []);
     console.log('stud', students);
     return (
         <div>
             <h2>List of Student</h2>
-            <ul>
+            <table>
+            <tr>
+                    <th>Student Number</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                </tr>
                 {students.map((student, index) => (
-                    <li key={index}>
-                       {student.studentNumber} {student.name} {student.surname}, 
-                    </li>
+                     <tr>
+                     <td>{student.studentNumber}</td>
+                     <td>{student.name}</td>
+                     <td>{student.surname}</td>
+                   </tr>
                 ))}
-            </ul>
+            </table>
         </div>
     );
 };
